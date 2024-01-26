2009520afe2799e69fd7662918840f36f45d38de
NhatTruong <truongmai750@gmail.com>
2024-01-26 10:04:00 +0700

    form review

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import styled from "styled-components";
 import Label from "../components/label/Label";
 import Input from "../components/input/Input";
 import { NavLink, useNavigate } from "react-router-dom";
@@ -10,21 +9,18 @@ import Field from "../components/field/Field";
 import Button from "../components/button/Button";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db } from "../firebase/firebase-config";
-import { toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import { addDoc, collection } from "firebase/firestore";
 import AuhenticationPage from "./AuhenticationPage";
 import InputTogglePassword from "../components/input/InputTogglePassword";
 
 const schema = yup.object({
-  fullname: yup.string().required("please enter your fullname"),
+  fullname: yup.string().required("please input your fullname"),
   email: yup
     .string()
-    .email("Please enter a valid email")
-    .required("please enter your email"),
-  password: yup
-    .string()
-    .required("please enter your password")
-    .min(8, "your password must be at least 8 character or greater"),
+    .required("please input your email")
+    .email("please input a valid email"),
+  password: yup.string().required("please input your pass word"),
 });
 
 const SignUpPage = () => {
@@ -32,15 +28,23 @@ const SignUpPage = () => {
   const {
     control,
     handleSubmit,
-    formState: { isValid, isSubmitting },
+    formState: { isValid, isSubmitting, errors },
   } = useForm({ mode: "onChange", resolver: yupResolver(schema) });
 
-  console.log("cong", 0.1 + 0.2);
+  useEffect(() => {
+    const arrayErros = Object.values(errors);
+    if (arrayErros.length > 0) {
+      toast.error(arrayErros[0]?.message, {
+        pauseOnHover: false,
+        delay: 0,
+      });
+    }
+  }, [errors]);
 
-  const handleSignUp = async (values) => {
+  const handleSign = async (values) => {
     if (!isValid) return null;
     await createUserWithEmailAndPassword(auth, values.email, values.password);
-    await updateProfile(auth.currentUser, {
+    await updateProfile(auth.currentUsser, {
       displayName: values.fullname,
     });
     const colRef = collection(db, "user");
@@ -49,16 +53,18 @@ const SignUpPage = () => {
       email: values.email,
       password: values.password,
     });
-    toast.success("Register successfully!");
+    toast.success("Register successfully");
     navigate("/");
   };
+
   useEffect(() => {
-    document.title = "RegisterPage";
+    document.tittle = "RegisterPage";
   }, []);
 
   return (
     <AuhenticationPage>
-      <form className="form" onSubmit={handleSubmit(handleSignUp)}>
+      {/* <form className="form" onSubmit={handleSubmit(handleSignUp)}> */}
+      <form className="form" onSubmit={handleSubmit(handleSign)}>
         <Field>
           <Label htmlFor="fullname" className="label">
             FullName
